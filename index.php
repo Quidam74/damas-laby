2b8d8d6f8a2ead38bfa28fdf4cba9a5d139af823
Unknown <toutlemonde74@gmail.com>
2018-10-09 08:50:15 +0200

    empty template

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html> 
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+
+<header>
+</header>
+
+<section>
+</section>
+
+<footer>
+</footer>
+
+
+
+<script type='text/javascript' src='js/jquery.js'></script>
+<script type='text/javascript' src='js/jquery-ui.js'></script>
+<script type='text/javascript' src='js/main.js'></script>
+</body>
+</html>
